49bb54cc84b10d81c9a32d39c289b3d8f67d41ec
Izaan Qaiser <izaanqaiser05@gmail.com>
2022-07-07 15:30:29 -0400

    tuff

---
+++ src/components/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { GoogleOutlined, FacebookOutlined} from '@ant-design/icons';
+
+const Login = () => {
+    return (
+      <div id ="login-page">
+
+      </div>  
+
+    );
+}
+
+export default Login;
